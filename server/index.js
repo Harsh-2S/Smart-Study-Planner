9803f515b55bae72280132351c32dfd538316ae2
Harsh-2S <hs648047@gmail.com>
2026-07-04 13:42:50 +0530

    Fix Express 5 wildcard routing compatibility

--- server/index.js
+++ server/index.js
@@ -116,7 +116,7 @@ ${notes}`
 const distPath = path.join(__dirname, '../dist')
 if (fs.existsSync(distPath)) {
   app.use(express.static(distPath))
-  app.get('*', (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'))
   })
 }
